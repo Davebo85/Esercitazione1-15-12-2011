9577fd7eb2fbc3958ae6e52ca4ed551793ef2c50
Davebo85 <bossi.davide@gmail.com>
2011-12-19 10:43:31 +0100

    Prova new branch - Scrittuera file di log

--- RisorseXML/src/com/risorsexml/RisorseXMLActivity.java
+++ RisorseXML/src/com/risorsexml/RisorseXMLActivity.java
@@ -1,22 +1,32 @@
 package com.risorsexml;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.app.Activity;
 import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.format.Time;
 import android.util.Log;
+import android.widget.Toast;
 
 public class RisorseXMLActivity extends Activity {
 	/** Called when the activity is first created. */
-	
+
+	File sdcard = Environment.getExternalStorageDirectory();
+	File file = new File(sdcard,"log.txt");
+
 	@Override
-	
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		creafile();
 		XmlResourceParser parser = getResources().getXml(R.xml.compilation);
 		try {
 			int eventType = parser.getEventType();
@@ -27,12 +37,14 @@ public class RisorseXMLActivity extends Activity {
 						String id = parser.getAttributeValue(0);
 						String str = "Brano:" + id;
 						Log.d("XML PARSER",str);
+						scrivi(1,str);
 					}
 				}
 				else if (eventType == XmlResourceParser.TEXT) {
 					String elementValue = parser.getText();
 					String str = elementValue;
 					Log.d("XML PARSER",str);
+					scrivi(2,str);
 				}
 				eventType = parser.next();
 			}
@@ -42,6 +54,45 @@ public class RisorseXMLActivity extends Activity {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
 
+	public void creafile() {
+		if (!file.exists())
+		{
+			try
+			{
+				file.createNewFile();
+			} 
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		else {
+			try
+			{
+				file.delete();
+				file.createNewFile();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void scrivi(int num, String text) {
+		try {
+			FileOutputStream fOut = new FileOutputStream(file);
+			OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+			myOutWriter.append(num + Time.MONTH_DAY + " " + Time.MONTH + " "
+					+ Time.YEAR + "/" + Time.HOUR + " " +
+					Time.MINUTE + " " + Time.SECOND + " --> " + text);
+			myOutWriter.close();
+			fOut.close();
+			Toast.makeText(getBaseContext(),"Done writing SD 'log.txt'",Toast.LENGTH_SHORT).show();
+		} catch (Exception e) {
+			Toast.makeText(getBaseContext(), e.getMessage(),Toast.LENGTH_SHORT).show();
+		}
 	}
 }
